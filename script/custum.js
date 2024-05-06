c61476f4927a0841877a4a30cde12ff314c58910
abdusamad8575 <samadpts786313@gmail.com>
2024-05-06 21:11:20 +0530

    updation

--- script/custum.js
+++ script/custum.js
@@ -61,10 +61,32 @@ function scrollFunction() {
 
 function showPopup(serviceName) {
     const serviceImages = {
-        'service1': ['images/m1.jpg', 'images/m2.jpg', 'images/m3.jpg'],
-        'service2': ['images/m4.jpg', 'images/m5.jpg', 'images/m6.jpg'],
-        'service3': ['images/m7.jpg', 'images/m8.jpg', 'images/m9.jpg'],
-        'service4': ['images/m2.jpg', 'images/m4.jpg', 'images/m8.jpg']
+        'service1': ['images/restorents/abhishek 1.jpg', 'images/restorents/abhishek 2.jpg', 'images/restorents/abhishek 3.jpg', 'images/restorents/4.jpg'],
+        'service2': ['images/restorents/brandclub 1.jpg', 'images/restorents/brandclub 2.jpg', 'images/restorents/brandclub 3.jpg', 'images/restorents/brandclub 4.jpg'],
+        'service3': ['images/restorents/delfish1.jpg', 'images/restorents/delfish2.jpg', 'images/restorents/delfish3.jpg', 'images/restorents/delfish4.jpg'],
+        'service4': ['images/restorents/deumotion 5.jpg', 'images/restorents/deumotion 1.jpg', 'images/restorents/deumotion 3.jpg', 'images/restorents/deumotion 4.jpg', 'images/restorents/deumotion 6.jpg'],
+        'service5': ['images/restorents/horticorp1.jpg', 'images/restorents/horticorp2.jpg', 'images/restorents/horticorp3.jpg', 'images/restorents/horticorp4.jpg'],
+        'service6': ['images/restorents/kunjikkante1.jpg', 'images/restorents/kunjikkante2.jpg', 'images/restorents/kunjikkante3.jpg', 'images/restorents/kunjikkante4.jpg'],
+        'service7': ['images/restorents/old school3.jpg', 'images/restorents/old school1.jpg', 'images/restorents/old school2.jpg', 'images/restorents/old school4.jpg'],
+        'service8': ['images/restorents/salwa 1.jpg', 'images/restorents/salwa 2.jpg', 'images/restorents/salwa 3.jpg', 'images/restorents/salwa 4.jpg'],
+        'service9': ['images/restorents/cleareye.ai6.jpg', 'images/restorents/cleareye.ai1.jpg', 'images/restorents/cleareye.ai2.jpg', 'images/restorents/cleareye.ai3.jpg', 'images/restorents/cleareye.ai4.jpg', 'images/restorents/cleareye.ai5.jpg', 'images/restorents/cleareye.ai7.jpg'],
+        'service10': ['images/restorents/talent orange1.jpg', 'images/restorents/talent orange2.jpg', 'images/restorents/talent orange3.jpg', 'images/restorents/talent orange4.jpg', 'images/restorents/talent orange5.jpg', 'images/restorents/talent orange6.jpg'],
+        'service11': ['images/restorents/green1.jpg', 'images/restorents/green2.jpg', 'images/restorents/green3.jpg', 'images/restorents/green4.jpg', 'images/restorents/green5.jpg', 'images/restorents/green6.jpg'],
+        'service20': ['images/restorents/DR FACE DENTAL1.jpg', 'images/restorents/DR FACE DENTAL2.jpg', 'images/restorents/DR FACE DENTAL3.jpg', 'images/restorents/DR FACE DENTAL4.jpg','images/restorents/DR FACE DENTAL5.jpg'],
+
+        'service12': ['images/restorents/Cricket shack1.jpg', 'images/restorents/Cricket shack2.jpg', 'images/restorents/Cricket shack3.jpg', 'images/restorents/Cricket shack4.jpg', 'images/restorents/Cricket shack5.jpg', 'images/restorents/Cricket shack6.jpg'],
+        
+        
+        
+        
+        'service13': ['images/restorents/MR HARI KAIMANAM1.jpg', 'images/restorents/MR HARI KAIMANAM2.jpg', 'images/restorents/MR HARI KAIMANAM3.jpg', 'images/restorents/MR HARI KAIMANAM4.jpg'],
+        'service14': ['images/restorents/PRATHIBHA 1.jpg', 'images/restorents/PRATHIBHA 2.jpg', 'images/restorents/PRATHIBHA 3.jpg', 'images/restorents/PRATHIBHA 4.jpg', 'images/restorents/PRATHIBHA 5.jpg', 'images/restorents/PRATHIBHA 6.jpg'],
+        'service15': ['images/restorents/RAJESH PEROORKADA1.jpg', 'images/restorents/RAJESH PEROORKADA2.jpg', 'images/restorents/RAJESH PEROORKADA3.jpg'],
+        'service16': ['images/restorents/MRS REIBY1.jpg', 'images/restorents/MRS REIBY2.jpg', 'images/restorents/MRS REIBY3.jpg', 'images/restorents/MRS REIBY4.jpg', 'images/restorents/MRS REIBY5.jpg'],
+        'service17': ['images/restorents/MRS SANJANA1.jpg', 'images/restorents/MRS SANJANA2.jpg', 'images/restorents/MRS SANJANA3.jpg', 'images/restorents/MRS SANJANA4.jpg','images/restorents/MRS SANJANA5.jpg'],
+        'service18': ['images/restorents/MR SHAHUL1.jpg', 'images/restorents/MR SHAHUL2.jpg', 'images/restorents/MR SHAHUL3.jpg', 'images/restorents/MR SHAHUL4.jpg','images/restorents/MR SHAHUL5.jpg','images/restorents/MR SHAHUL6.jpg'],
+        'service19': ['images/restorents/SUMESH KOVALAM1.jpg', 'images/restorents/SUMESH KOVALAM2.jpg', 'images/restorents/SUMESH KOVALAM3.jpg', 'images/restorents/SUMESH KOVALAM4.jpg','images/restorents/SUMESH KOVALAM5.jpg'],
+        
     };
 
     const images = serviceImages[serviceName];
@@ -84,8 +106,8 @@ function showPopup(serviceName) {
             const img = document.createElement('img');
             img.src = imageUrl;
             img.alt = serviceName;
-            img.style.width = '300px'; // Set image width to 300px
-            img.style.marginBottom = '10px';
+            img.style.width = '250px'; // Set image width to 300px
+            img.style.margin = '5px';
             popup.appendChild(img);
         });
 
